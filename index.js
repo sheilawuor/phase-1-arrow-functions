2647e73cf834379b2d66035f0d93498239d10395
sheilawuor <sheilawuor01@gmail>
2025-09-17 15:33:58 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression
+const divide = function(a, b) {
+  return a / b;
+};
+
+// Arrow function with one parameter
+const square = x => x * x;
+
+// Arrow function with two parameters
+const add = (a, b) => a + b;
